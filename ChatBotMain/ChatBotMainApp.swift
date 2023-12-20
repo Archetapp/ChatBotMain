c90ac525872798ecdba20d0a6bcd85b29cedeacc
Jared Davidson (Archetapp) <archetapp@gmail.com>
2023-12-19 18:12:16 -0700

    Initial Commit

---
+++ ChatBotMain/ChatBotMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotMainApp.swift
+//  ChatBotMain
+//
+//  Created by Jared Davidson on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatBotMain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatBotMain
+//
+//  Created by Jared Davidson on 12/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
